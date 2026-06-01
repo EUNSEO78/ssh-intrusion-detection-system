095316e951ba1d7a43383008f95a09ec8ed9d647
EUNSEO78 <dmstjalstj4@gmail.com>
2026-06-01 13:08:41 +0000

    feat: count failed SSH login attempts by IP

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-# print("SSH Defense System Start")
-
 import re
 
 
 file_path = "/var/log/auth.log"
 search_string = "Failed password for"
+failed_attempts = {}
 
 with open(file_path, "r") as file:
     for line in file:
@@ -16,7 +15,15 @@ with open(file_path, "r") as file:
             ip_address = re.search(ip_regex, failed_log)
             print(ip_address)
             if ip_address:
-                print("Failed IP Address:", ip_address.group())
+                ip = ip_address.group()
+                print("Failed IP Address:", ip)
+                # 실패한 IP 주소를 딕셔너리에 저장, 횟수 카운트
+                if ip not in failed_attempts:
+                    failed_attempts[ip] = 1
+                else:
+                    failed_attempts[ip] += 1
+                print("Failed Attempts List:", failed_attempts)
+            
 
 
 
